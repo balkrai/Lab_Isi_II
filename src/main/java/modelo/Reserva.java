66766042a52de4a7b8d4fdfeffb41abb3bba71bf
vicen <vicen@10.20.54.196>
2023-11-09 09:39:26 +0100

    No subi correctamente las funciones implementadas en la clase reserva

--- src/main/java/modelo/Reserva.java
+++ src/main/java/modelo/Reserva.java
@@ -1,34 +1,43 @@
 package modelo;
 
+import java.time.Instant;
 import java.util.Date;
 
 public class Reserva {
-    private int id;
     private Date fechaInicio, fechaFin;
+    private boolean llegada, salida;
     private Cliente cliente;
     private Parcela parcela;
+    private int id;
     public Reserva(int id, Date fechaInicio, Date fechaFin, Cliente cliente, 
-            Parcela parcela)
+                   Parcela parcela)
     {
-        this.id = id;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
         this.cliente = cliente;
         this.parcela = parcela;
+        this.llegada = false;
+        this.salida = false;
+        this.id = id;
         parcela.anyadirReserva(this);
     }
+    
     
     public Date getFechaInicio()
     {
         return fechaInicio;
     }
-    public float getPrecio()
-    {
-        return parcela.getPrecio();
-    }
     public Date getFechaFin()
     {
         return fechaFin;
+    }
+    public boolean getLlegada()
+    {
+        return llegada;
+    }
+    public boolean getSalida()
+    {
+        return salida;
     }
     public Cliente getCliente()
     {
@@ -37,9 +46,33 @@ public class Reserva {
     public Parcela getParcela()
     {
         return parcela;
-    }      
+    }
+    public float getPrecio()
+    {
+        return parcela.getPrecio();
+    }
     public int getId()
     {
         return id;
+    }
+    public boolean setLlegada()
+    {
+        Date fechaLlegada = Date.from(Instant.now());
+        if(fechaLlegada.compareTo(this.fechaInicio) >= 0 && 
+           fechaLlegada.compareTo(this.fechaFin) < 0)
+        {
+            llegada = true;
+            return true;
+        }
+        return false;
+    }
+    public boolean setSalida()
+    {
+        if(llegada)
+        {
+            salida = true;
+            return true;
+        }
+        return false;
     }
 }
